44156bcf1aa001d5a7fca2f9962dea0131b3d122
anjalibansal87 <58892859+anjalibansal87@users.noreply.github.com>
2022-10-02 22:21:30 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-print("Hello");
-print("Hello Jenkins");
+from flask import Flask
+server = Flask(__name__)
+
+@server.route("/")
+ def hello():
+    return "Hello Jenkins!"
+
+if __name__ == "__main__":
+   server.run(host='0.0.0.0')
